b25c02e038fca17651fb6d31d22805451de60aff
Roberto Rodriguez <roberto@MacBook-Air-de-Roberto.local>
2021-07-29 14:17:09 -0500

    primer commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return(
+    <>
+      <Text>Hola</Text>
+    </>
+  )
+};
+
+
+
+
+export default App;
